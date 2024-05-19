90e77c1be11e7065114db5b5d79cec905941a8e0
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-05-19 20:12:37 +0300

    feat: Add code in files config.ts, errorHandler.ts and requestlogger.ts

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,18 +1,16 @@
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
-// // environment
-// const NODE_ENV: string = process.env['NODE_ENV'] || 'development';
+const NODE_ENV: string = process.env['NODE_ENV'] || 'development';
 
-// // application
-// const PORT: number = parseInt(process.env['PORT'] || 4000, 10);
+const PORT: number = Number(process.env['PORT'] || 4000);
 
-// // JWT
-// const JWT_SECRET_KEY: string = process.env['JWT_SECRET_KEY'] || 'secret-key';
 
-// // database
-// const MONGO_CONNECTION_STRING: string | undefined =
-//   process.env['MONGO_CONNECTION_STRING'] || 'your-mongo-db-connection-string';
+const JWT_SECRET_KEY: string = process.env['JWT_SECRET_KEY'] || 'secret-key';
 
-// export { PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY };
+
+const MONGO_CONNECTION_STRING: string | undefined =
+process.env['MONGO_CONNECTION_STRING'] || 'your-mongo-db-connection-string';
+
+export { PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY };
